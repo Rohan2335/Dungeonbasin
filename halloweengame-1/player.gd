ea9a244658d5a6bbc12a08496dba20b37f9818b1
Rohan2335 <thunderr.2335@gmail.com>
2025-10-18 19:55:25 +0530

    Added jump to the annimation
    added wasd keybinds and the spacebar triggers the jump annimation

--- halloweengame-1/player.gd
+++ halloweengame-1/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 @onready var annimated_sprite = $AnimatedSprite2D
+@onready var timer: Timer = $Timer
 
 
 
@@ -13,21 +14,31 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		annimated_sprite.play("jump")
+		timer.start()
+	if Input.is_action_just_pressed("jump"):
+		print(annimated_sprite.sprite_frames.get_animation_names())
+		annimated_sprite.play("jump")
+	if Input.is_action_just_pressed("jump"):
 		annimated_sprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
+		
 		velocity.x = direction * SPEED
 		annimated_sprite.play("walk")
 		annimated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		annimated_sprite.play("idle")
-
+	if not is_on_floor():
+		# Jumping or falling — use jump animation
+		annimated_sprite.play("jump")
 
 	move_and_slide()
